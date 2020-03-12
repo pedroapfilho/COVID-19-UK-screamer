a5288cd6d3ad9185ebab2162d00004f99d67ad64
Pedro Filho <pedro@filho.me>
2020-03-12 22:34:09 +0000

    adding script

---
+++ README.md
@@ -0,0 +1,17 @@
+# COVID-19-UK Screamer
+
+It screams when it finds more cases in your area
+
+You need a `.env` with:
+
+```
+AREA="YOUR AREA HERE"
+```
+
+You can find the correct name of your area here: https://www.gov.uk/government/publications/coronavirus-covid-19-number-of-cases-in-england/coronavirus-covid-19-number-of-cases-in-england
+
+To run the script, you simply run:
+
+```
+node .
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+var player = require("play-sound")((opts = {}));
+
+require("dotenv").config();
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+  const page = await browser.newPage();
+
+  await page.goto(
+    "https://www.gov.uk/government/publications/coronavirus-covid-19-number-of-cases-in-england/coronavirus-covid-19-number-of-cases-in-england",
+    {
+      waitUntil: "networkidle0"
+    }
+  );
+
+  let numberOfCases;
+
+  console.log("Running");
+
+  while (true) {
+    const data = await page.$$eval("table tr td", tds =>
+      tds.map(td => {
+        return td.innerHTML;
+      })
+    );
+
+    const indexOfArea = data.indexOf(process.env.AREA);
+
+    const newNumberOfCases = Number(data[indexOfArea + 1]);
+
+    if (newNumberOfCases > numberOfCases) {
+      console.log("FUCK");
+      player.play("scream.mp3", err => {
+        if (err) throw err;
+      });
+    }
+
+    numberOfCases = newNumberOfCases;
+
+    await page.reload();
+  }
+})();
